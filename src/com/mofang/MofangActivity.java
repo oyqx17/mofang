6200348ece9c08e724fe534159dee037614dc36e
oyqx <qx.ouyang@gmail.com>
2011-06-28 15:10:26 +0800

    15:09

--- src/com/mofang/MofangActivity.java
+++ src/com/mofang/MofangActivity.java
@@ -1,13 +1,38 @@
 package com.mofang;
 
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
 import android.app.Activity;
+import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 
-public class MofangActivity extends Activity {
+public class MofangActivity extends Activity implements GLSurfaceView.Renderer {
+    private GLSurfaceView mView;
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        mView = new GLSurfaceView(getApplicationContext());
+        setContentView(mView);
+    }
+
+    @Override
+    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl, int width, int height) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl) {
+        // TODO Auto-generated method stub
+
     }
 }
